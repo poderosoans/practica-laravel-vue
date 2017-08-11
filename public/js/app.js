196daeddfcc3c32ca4ae753d56c52e533c536224
Roger Pomacarhua <poderosoans@hotmail.com>
2017-08-11 11:49:17 -0500

    Prueba rápida de funcionacionamiento de Vue

--- public/js/app.js
+++ public/js/app.js
@@ -11661,3 +11661,20 @@ return /******/ (function(modules) { // webpackBootstrap
 });
 ;
 //# sourceMappingURL=axios.map
+var urlUsers ='https://jsonplaceholder.typicode.com/users';
+ 			new Vue({
+ 				el: '#main',
+ 				created: function(){
+ 					this.getUsers();
+ 				},
+ 				data: {
+ 					lists: []
+ 				},
+ 				methods:{
+ 					getUsers: function(){
+ 						axios.get(urlUsers).then(response =>{
+ 							this.lists = response.data
+ 						});
+ 					}
+ 				}
+});
